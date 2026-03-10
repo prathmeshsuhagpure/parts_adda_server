3f03e297930eca34a6b65445d3bbadcef4ac6da6
Prathmesh Suhagpure <prathmeshsuhagpure3@gmail.com>
2026-03-10 23:12:35 +0530

    Fixed Dealer Routes

--- src/routes/dealerRoutes.js
+++ src/routes/dealerRoutes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const dealerController = require("../controllers/dealerController");
+const { protect } = require("../middlewares/auth");
 
 router.post("/apply", dealerController.applyDealer);
-router.get("/status", dealerController.getDealerStatus);
+router.get("/status", protect, dealerController.getDealerStatus);
 
 module.exports = router;
